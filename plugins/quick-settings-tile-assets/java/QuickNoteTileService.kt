63936bc43a6135255375cfb9d61b81279a95f8bd
tranquil-tr0 <tranquiltr0@proton.me>
2025-06-16 00:29:48 -0700

    tile to launch my app (hopefully it works)

--- plugins/quick-settings-tile-assets/java/QuickNoteTileService.kt
+++ plugins/quick-settings-tile-assets/java/QuickNoteTileService.kt
@@ -12,50 +12,24 @@ class QuickNoteTileService : TileService() {
     override fun onClick() {
         super.onClick()
         
-        // // Get the quick note filename from AsyncStorage
-        // val quickNoteFilename = AsyncStorageHelper.getQuickNoteUri(this)
+        // Get the quick note filename from AsyncStorage
+        val quickNoteFilename = AsyncStorageHelper.getQuickNoteUri(this)
         
-        // val deepLink = if (quickNoteFilename != null) {
-        //     // Extract just the filename from the URI for the noteId parameter
-        //     val filename = UriHelper.extractFilenameFromUri(quickNoteFilename)
-        //     "linknotes://editor?mode=edit&noteId=$filename"
-        // } else {
-        //     "linknotes://settings?showToast=true&message=No%20quick%20note%20selected.%20Please%20select%20a%20note%20in%20Settings."
-        // }
+        val deepLink = if (quickNoteFilename != null) {
+            // Extract just the filename from the URI for the noteId parameter
+            val filename = UriHelper.extractFilenameFromUri(quickNoteFilename)
+            "linknotes://editor?mode=edit&noteId=$filename"
+        } else {
+            "linknotes://settings?showToast=true&message=No%20quick%20note%20selected.%20Please%20select%20a%20note%20in%20Settings."
+        }
         
-        // // Launch the app with the deep link
-        // unlockAndRun {
-        //     val intent = Intent(Intent.ACTION_VIEW, Uri.parse(deepLink)).apply {
-        //         flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-        //         setPackage(packageName)
-        //     }
-        //     startActivity(intent)
-        // }
+        // Launch the app with the deep link
         unlockAndRun {
-            // Collapse the notification shade
-            val collapseStatusBarIntent = Intent("com.android.systemui.statusbar.COLLAPSE_PANEL")
-            collapseStatusBarIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND)
-            sendBroadcast(collapseStatusBarIntent)
-
-            // Try multiple approaches to launch Pinterest
-            try {
-                // First try with explicit package
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("pinterest://pin/9/")).apply {
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    setPackage("com.pinterest")
-                }
-                startActivity(intent)
-            } catch (e: Exception) {
-                // Fallback to web URL if Pinterest app is not installed
-                try {
-                    val fallbackIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pinterest.com/")).apply {
-                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    }
-                    startActivity(fallbackIntent)
-                } catch (e2: Exception) {
-                    // Log error or show toast if needed
-                }
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(deepLink)).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                setPackage(packageName)
             }
+            startActivity(intent)
         }
     }
 }
